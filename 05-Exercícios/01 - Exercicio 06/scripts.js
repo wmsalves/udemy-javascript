7706132ff5ff7723f58dde345e4d2472d438657d
Veidoido <wemersoncontas22@gmail.com>
2023-03-16 08:40:46 -0300

    Fourth Module

---
+++ 05-Exercícios/01 - Exercicio 06/scripts.js
@@ -0,0 +1,9 @@
+let nome = "Wemerson";
+let idade = 22;
+let cidade = "Belo Horizonte";
+let cnh = true;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof cidade);
+console.log(typeof cnh);
---
+++ 05-Exercícios/05 - Exercicio 10/scripts.js
@@ -0,0 +1,7 @@
+let vel = 60;
+
+if(vel > 80) {
+  console.log("Multa por excesso de velocidade");
+} else {
+  console.log("Não levou multa");
+}
